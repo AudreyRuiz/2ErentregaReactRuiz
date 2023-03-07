cacdcf368c80f0f7459520c4f2726b48e551889d
AudreyRuiz <audreyrd@outlook.com>
2023-03-06 22:50:56 -0600

    first commit

---
+++ primer-proyecto/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {/* Navbar */}
+      {/* Main */}
+      {/* Footer */}
+      <h1>Hola mundo</h1>
+      <h2>Mi</h2>
+      <h2>Primer</h2>
+      <h2>Proyecto</h2>
+      <h2>React</h2>
+      <ul>
+        <li>Manzana</li>
+        <li>Pera</li>
+        <li>uva</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
